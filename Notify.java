6155013e2403a7afbedecc33980f58c62c0b3426
Sean Hellum <seanhellum45@gmail.com>
2020-01-04 16:43:50 +0000

    init

---
+++ Notify.java
@@ -0,0 +1,10 @@
+/**
+ * Notify
+ */
+import javax.swing.JOptionPane;
+public class Notify {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Check Youe Postue");
+    }
+}
